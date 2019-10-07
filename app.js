9b436b81a89213b892de78c1b9f3353919464cda
ElementG9 <elementg9dev@gmail.com>
2019-10-07 11:52:46 -0600

    Basic website

---
+++ app.js
@@ -0,0 +1,29 @@
+/* - Configuration - */
+// Create the app.
+const app = require('express')();
+// Import dependencies.
+const helmet = require('helmet');
+const compression = require('compression');
+// Configure dependencies.
+app.use(helmet());
+app.use(compression());
+app.set('json spaces', 2);
+app.set('view engine', 'pug');
+app.set('views', `${__dirname}/views/`);
+
+/* - Routes - */
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/pivot', (req, res) => {
+  res.render('pivot/index');
+});
+// Serve static files.
+app.get('/css/:file', (req, res) => {
+  res.sendFile(`${__dirname}/public/css/${req.params.file}`);
+});
+
+/* - Listening - */
+app.listen(3000, () => {
+  console.log('Server listening on port 3000.');
+});
