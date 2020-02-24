62df22f2a1b982df76fa1c9de5b3464e71153339
ElementG9 <garentyler@gmail.com>
2020-02-23 23:38:02 -0700

    Styling changes + Add Rust
    Center text and make mobile friendly. Add Rust and rewrite paragraphs

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = require('express')();
 // Import dependencies.
 const helmet = require('helmet');
 const compression = require('compression');
+const fs = require('fs');
 // Configure dependencies.
 app.use(helmet());
 app.use(compression());
@@ -19,10 +20,17 @@ app.get('/guides', (req, res) => {
   res.render('guides/index');
 });
 app.get('/guides/:guide', (req, res) => {
-  res.render(`guides/${req.params.guide}/index`);
+  if (fs.existsSync(`${__dirname}/views/guides/${req.params.guide}/index.pug`))
+    res.render(`guides/${req.params.guide}/index`);
+  else
+    res.status(404).render('404');
 });
+
+// Joke paths
 app.get('/50shadesofspacegray', (req, res) => {res.redirect('https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1313.TR11.TRC1.A0.H0.Xspace+gray+iphone+6s.TRS0&_nkw=space+gray+iphone+6s&_sacat=0');});
 app.get('/millenialsgetoffendedeasily', (req, res) => res.send('ok boomer'));
+app.get('/loss', (req, res) => res.send('|\n||\n||\n|_'));
+
 // Serve static files.
 app.get('/favicon.ico', (req, res) => {
   res.sendFile(`${__dirname}/public/favicon.ico`);
@@ -34,6 +42,14 @@ app.get('/css/:file', (req, res) => {
   res.sendFile(`${__dirname}/public/css/${req.params.file}`);
 });
 
+app.get('/404', (req, res) => {
+  res.status(404).render('404');
+});
+
+// 404 error.
+app.use(function(req, res, next) {
+  res.status(404).render('404');
+});
 /* - Listening - */
 app.listen(3000, () => {
   console.log('Server listening on port 3000.');
